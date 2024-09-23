ee300b5793a63f4a7df12854e3202784d20ab86f
Pradnya Kumbhar <kumbharpradnyaa10@gmail.com>
2024-09-23 16:54:13 +0530

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: process.env.PORT || 5173, // Default to 5173 if PORT isn't set
+    host: true, // To expose the network
+  },
 });
